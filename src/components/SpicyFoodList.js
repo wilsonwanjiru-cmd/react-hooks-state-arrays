3e73fe620e9a6a2dc61389ef0138fc6e793b7f32
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-08-28 15:12:39 +0300

    <React State and Arrays>

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,12 +6,33 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+  }
+
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+  }
+
+  function handleIncrementHeat(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      }
+      return food;
+    });
+    setFoods(newFoodArray);
   }
 
   const foodList = foods.map((food) => (
     <li key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+      <button onClick={() => handleLiClick(food.id)}>Remove</button>
+      <button onClick={() => handleIncrementHeat(food.id)}>Increase Heat</button>
     </li>
   ));
 
